2274855fe1f9c8a5e3a5ab1c32cae160a9215995
Op-harsh <gamer6203788@gmail.com>
2026-04-10 22:04:35 +0530

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,5 @@
+# --- Handlers Package ---
+# Ye file 'handlers' folder ko ek Python module banati hai.
+# Iski wajah se hum main.py me asani se (from handlers.file_name import function_name) kar paate hain.
+
+# Tum isko pura khali bhi chhod sakte ho, isme koi logic likhne ki zaroorat nahi hai.
